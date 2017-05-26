048e811001ee1f1a76b9edbb08cda3000a25fd5e
seifer8ff <brady.bcmp@gmail.com>
2017-05-26 11:37:26 -0700

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var concat = require('gulp-concat');
+var path = require('path');
+var uglify = require('gulp-uglify');
+var stripDebug = require('gulp-strip-debug');
+var gulpIf = require('gulp-if');
+var useref = require('gulp-useref');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync').create();
+
+
+
+// Concatenate & Minify JS & CSS
+gulp.task('useref', function(){
+    return gulp.src('app/*.html')
+    .pipe(useref())
+    // strips debug and minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', stripDebug()))
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+// optimize images
+gulp.task('images', function(){
+  return gulp.src('app/img/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/img'))
+});
+
+// copy extras to dist
+gulp.task('extras', function() {
+  return gulp.src('app/*.+(png|xml|ico|json|svg)')
+  .pipe(gulp.dest('dist/'))
+})
+
+// setup server
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+// Watch Files For Changes and reload browser
+gulp.task('watch', ['browserSync'], function() {
+    gulp.watch('app/*.html', browserSync.reload); 
+    gulp.watch('app/js/*.js', browserSync.reload); 
+    gulp.watch('app/css/*.css', browserSync.reload); 
+});
+
+// build task
+gulp.task('build', function() {
+  gulp.run('useref'); 
+  gulp.run('extras'); 
+  gulp.run('images'); 
+});
+
+// Default Task
+gulp.task('default', ['watch']);
+
+
